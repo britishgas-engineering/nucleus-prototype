56b20f988f0e014d127c3e43b47fe63796ea8cb5
samthidny <andy.smith6@britishgas.co.uk>
2020-03-24 20:41:25 +0000

    add multi questions

--- bin/cli.js
+++ bin/cli.js
@@ -1,37 +1,55 @@
 #!/usr/bin/env node
 
-console.log('Hello');
 var shell = require("shelljs");
 var parseArgs = require("minimist")
 
-go();
+const prompts = require('prompts');
+ 
+const questions = [
+  {
+    type: 'select',
+    name: 'template',
+    message: 'Pick a template to base your project on',
+    choices: [
+        { title: 'Multi Part Form', description: 'A form with multi parts.', value: 'project' },
+        { title: 'Page Journey', description: 'A journey with pages.', value: 'project' },
+        { title: 'Foo Journey', description: 'A foo journey.', value: 'project' }
+    ]
+  },
+  {
+    type: 'text',
+    name: 'project',
+    message: 'Enter the name for your new project?  Use lowercase letters, separate words with hyphens, eg: "my-project"',
+    validate: (value) => value.length > 0 ? true : `Ypou didn't enter a name for your project`
+  }
+];
 
-function go() {
-
-  // Delete dist
-  shell.rm('-rf', 'dist/*')
-
-  // Get params
-  let args = parseArgs(process.argv.slice(2));
-  const command = args._[0];
-  const template = args._[1];
-  const project = args._[2];
+const go = (response) => {
+  
+  console.log('XXXXXXXXXX');
+  
+  const command = args._[0] || 'g';
+  const template = args._[1] || response.template;
+  const project = args._[2] || response.project;
 
   console.log('>>>>>> args ' + args._.length);
   const projectPath = 'src/' + project;
 
   if (!command) {
-    console.log('No command was entered.  Supported commnads are g (generate) only.');
+    console.log('No command was entered.  Supported commands are: g (generate) only.');
     return;
   }
   if (!template) {
-    console.log('No template was entered.  You must chose a template to base your project from.  eg "project"');
+    console.log('No template was entered.  You must choose a template to base your project on.  eg "project"');
     return;
   }
   if (!project) {
     console.log('No project name was entered.  You must chose a new name for your project.');
     return;
   }
+
+  // Delete dist
+  shell.rm('-rf', 'dist/*')
   
   // Copy template project to new project
   shell.rm('-rf', projectPath)
@@ -46,5 +64,20 @@ function go() {
 
 }
 
-
-
+// Get params
+let args = parseArgs(process.argv.slice(2));
+// If no args then ask questions
+if (args._.length === 0) {
+  if (args._.length === 0) {
+    const res = (async () => {
+      const response = await prompts(questions);
+      // Logic here
+      response.template = 'project';
+      go(response);
+      console.log(response); // => { value: 24 }
+    })();
+  }
+} else {
+  // Process with args
+  go();
+}
