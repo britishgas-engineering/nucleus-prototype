dd8c3880c9f6da3e8b5fbdd74e00b799f991dcc7
Scott Robinson <scott.robinson1@centrica.com>
2020-07-24 09:48:01 +0100

    Removed unrequired whenDefined promise blocks.

--- src/hs/home-care/script.js
+++ src/hs/home-care/script.js
@@ -23,54 +23,48 @@ window.hc = function () {
   }
 
   // Add giftcard banner
-  customElements.whenDefined('ns-panel').then(() => {
-    customElements.whenDefined('ns-lockup').then(() => {
-      customElements.whenDefined('ns-image').then(() => {
-        const image = document.querySelector('ns-image');
-        let p = document.createElement("p");
-        p.className = 'banner';
-        p.innerHTML = `
-          <style>
-            .banner {
-              position: relative;
-              margin: 0;
-              bottom: 0;
-              font-size: 1.5em;
-              line-height: 1.3em;
-              font-weight: bold;
-              padding: 20px 30px 20px 90px;
-              background: #FFDD57 url(/assets/hs/home-care/present.svg) no-repeat 30px 25px;
-              background-size: 35px 35px;
-            }
+  customElements.whenDefined('ns-lockup').then(() => {
+    customElements.whenDefined('ns-image').then(() => {
+      const image = document.querySelector('ns-image');
+      let p = document.createElement("p");
+      p.className = 'banner';
+      p.innerHTML = `
+        <style>
+          .banner {
+            position: relative;
+            margin: 0;
+            bottom: 0;
+            font-size: 1.5em;
+            line-height: 1.3em;
+            font-weight: bold;
+            padding: 20px 30px 20px 90px;
+            background: #FFDD57 url(/assets/hs/home-care/present.svg) no-repeat 30px 25px;
+            background-size: 35px 35px;
+          }
 
-            @media (min-width: 1024px) {
-              .banner {
-                position: absolute;
-              }
+          @media (min-width: 1024px) {
+            .banner {
+              position: absolute;
             }
-          </style>
-          ${image.innerHTML}`;
-        image.shadowRoot.appendChild(p);
-      });
+          }
+        </style>
+        ${image.innerHTML}`;
+      image.shadowRoot.appendChild(p);
     });
   });
 
   // Add expander highlights
-  customElements.whenDefined('ns-panel').then(() => {
-    customElements.whenDefined('ns-column').then(() => {
-      customElements.whenDefined('ns-product-card').then(() => {
-        customElements.whenDefined('ns-expander').then(() => {
-          const expanders = document.querySelectorAll('ns-expander[highlight="focus"]');
-          expanders.forEach((expander) => {
-            let style = document.createElement("style");
-            style.innerHTML = `
-              :host .heading-title {
-                background: #FFDD57;
-              }
-            `;
-            expander.shadowRoot.appendChild(style);
-          });
-        });
+  customElements.whenDefined('ns-product-card').then(() => {
+    customElements.whenDefined('ns-expander').then(() => {
+      const expanders = document.querySelectorAll('ns-expander[highlight="focus"]');
+      expanders.forEach((expander) => {
+        let style = document.createElement("style");
+        style.innerHTML = `
+          :host .heading-title {
+            background: #FFDD57;
+          }
+        `;
+        expander.shadowRoot.appendChild(style);
       });
     });
   });
